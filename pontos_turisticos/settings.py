222d2e5dbfb9f3c9259c51116ab2eaa94068b1ae
RodrigoMachado9 <rodrigo.machado3.14@hotmail.com>
2019-06-23 00:34:12 -0300

    commit diario

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -130,7 +130,6 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = 'imagens'
 MEDIA_URL = '/media/'
 
-
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
 
--- pontos_turisticos/wsgi.py
+++ pontos_turisticos/wsgi.py
@@ -15,7 +15,5 @@ from dj_static import Cling
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pontos_turisticos.settings')
 
 #todo envolvendo > get_wsgi_application, usando o Cling > avalia se a requisição se trata de um arquivo statico ou não.
-
 application = Cling(get_wsgi_application())
-
 
